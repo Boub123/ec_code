0032510bfbf965d47adbbccfc0c1d1ec6d4f05a4
yatera <boubacar.yatera@edu.itescia.fr>
2020-06-24 23:50:07 +0200

    contact form in index page and user compte

---
+++ ec-code-2020-codflix-php-master/controller/contactController.php
@@ -0,0 +1,27 @@
+<?php
+//file to send mail
+function contact($post){
+    $email = $post['email'];
+    $message = $post['message'];
+    //mail verification
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        echo  "L'adresse email '$email' est considérée comme invalide.";
+    }//message verification
+    elseif (empty($message))
+    {
+        echo "le message est vide, veuillez écrire quelque chose.";
+    }
+    else{
+        sendMail($email, $message);
+        echo "Nous avons recu votre mail, nous vous répondrons dés que possible.";
+    }
+
+    require('view/contactView.php');
+}
+//send mail
+function sendMail($senderMail, $message)
+{
+    $MailSubject ="Contact via formulaire";
+    $Mailheader ='FROM: '.$senderMail;
+}
--- ec-code-2020-codflix-php-master/index.php
+++ ec-code-2020-codflix-php-master/index.php
@@ -4,6 +4,8 @@ require_once( 'controller/homeController.php' );
 require_once( 'controller/loginController.php' );
 require_once( 'controller/signupController.php' );
 require_once( 'controller/mediaController.php' );
+//add page
+require_once('controller/contactController.php');
 
 /**************************
 * ----- HANDLE ACTION -----
@@ -34,6 +36,9 @@ if ( isset( $_GET['action'] ) ):
 
     break;
 
+      case 'contact':
+          if(!empty($_POST)) contact($_POST);
+          else require('view/contactView.php');
   endswitch;
 
 else:
